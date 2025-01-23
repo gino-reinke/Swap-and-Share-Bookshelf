290f4c3c06da7d941d83cd0f8f75aa0f51bf1a0c
Gino Reinke <ginpreinke@gmail.com>
2025-01-23 17:53:10 -0500

    Test change

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
+  console.log("App component rendered");
   return <div className={styles.App}>
     <Navbar />
+
+    // test the Navbar component
   </div>
   
 }
