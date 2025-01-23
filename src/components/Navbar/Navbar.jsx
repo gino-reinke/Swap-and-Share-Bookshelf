2be4bed788d4fac07972f08b5cd23c136ac2def9
Gino Reinke <ginpreinke@gmail.com>
2025-01-22 20:46:23 -0500

    Created basic navbar, no links to pages yet

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,10 +7,37 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-
-      {/* TODO: change title element to custom logo */}
-      <a className={styles.title} href="/">Swap & Share Bookshelf</a> 
-
+      <a className={styles.title} href="/">Swap & Share Bookshelf</a>
+      <div className={styles.menu}>
+        <img 
+          className={styles.menuBtn} 
+          src={
+            menuOpen 
+              ? getImageUrl("nav/menu.png") // can change to close icon maybe
+              : getImageUrl("nav/menu.png")
+          } 
+          alt="menu-button" 
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={() => setMenuOpen(false)}>
+            <li><a href="#Marketplace">Marketplace</a></li>
+            <li><a href="#tradingtips">Trading Tips</a></li>
+            <li><a href="#about">About</a></li>
+            <li>
+              <input 
+                type="text" 
+                placeholder="Search..." 
+                className={styles.searchBar} 
+              />
+            </li>
+            <li className={styles.icons}>
+              <img src={getImageUrl("nav/heart.png")} alt="heart" className={styles.icon} />
+              <img src={getImageUrl("nav/inbox.png")} alt="inbox" className={styles.icon} />
+              <img src={getImageUrl("nav/account.png")} alt="account" className={styles.icon} />
+            </li>
+        </ul>
+      </div>
     </nav>
     )
 
