a5dad144fe297d2ea1f993564c07656cc3ea0cd5
tirth1995 <hw8844@wayne.edu>
2025-02-14 00:42:36 -0500

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
-import styles from './About.module.css';
-import { getImageUrl } from '../../utils';
+import React from "react";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
 
 export const About = () => {
-
-    return (
-        <section className={styles.container}>
-
-        </section>
-    );
+  return (
+    <section className={styles.container}>
+      <h1>About Swap & Share Bookshelf</h1>
+      <p>
+        Swap & Share Bookshelf is a community-driven platform that allows book lovers to 
+        exchange books online easily and conveniently. Whether you're looking to discover new 
+        stories or share your favorite reads, our platform connects readers from all around 
+        to swap books for free.
+      </p>
+      <p>
+        Simply list the books you want to swap, browse available books, and connect with 
+        fellow readers to arrange an exchange. Join us in promoting sustainable reading 
+        and building a global book-sharing community!
+      </p>
+      
+      {/*Image Tag */}
+      <img 
+        src={getImageUrl("login/loginLibrary.png")} 
+        alt="Little Library Image" 
+        className={styles.heroImg}
+      />
+    </section>
+  );
 };
