677d734232bd8f3453fce141fb42287cc42c7f8a
Gino Reinke <ginpreinke@gmail.com>
2025-03-14 13:53:55 -0400

    Truncated description of listings for consistent layout.
    Adjusted size of create listing modal.

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -5,6 +5,11 @@ import { getImageUrl } from '../../utils';
 const Listing = ({ listing }) => {
   const { title, description, state, city, image } = listing;
 
+  // Function to truncate the description
+  const truncateText = (text, maxLength) => {
+    return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.column}>
@@ -17,13 +22,15 @@ const Listing = ({ listing }) => {
             />
           )}
           <img
-            src= {getImageUrl("listing/heart.png")}
+            src={getImageUrl("listing/heart.png")}
             className={styles.favoriteIcon}
             alt="Favorite Icon"
           />
         </div>
         <span className={styles.bookTitle}>{title}</span>
-        <span className={styles.bookDescription}>{description}</span>
+        <span className={styles.bookDescription}>
+          {truncateText(description, 132)}
+        </span>
         <span className={styles.bookLocation}>{city}, {state}</span>
         <div className={styles.detailsRow}>
           <button className={styles.detailsButton} onClick={() => alert("Pressed!")}>
