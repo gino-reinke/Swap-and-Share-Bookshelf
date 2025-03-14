867796557bbe64243a659d2cd1c3201535a267a4
Gino Reinke <ginpreinke@gmail.com>
2025-03-14 12:40:52 -0400

    Fixed footer to appear at bottom of page and have correct width. Changed navbar icons to svg files and corrected size

--- src/pages/MarketplacePage.jsx
+++ src/pages/MarketplacePage.jsx
@@ -1,13 +1,20 @@
 import styles from '../App.module.css'
 import { Navbar } from '../components/Navbar/Navbar'
 import { Marketplace } from '../components/Marketplace/Marketplace'
+import { Footer } from '../components/Footer/Footer'
 import "../vars.css"
 
 
 function MarketplacePage() {
   return <div className={styles.App}>
-    <Navbar />
-    <Marketplace />
+        <div className={styles.App}>
+          <Navbar />
+          <div className={styles.Main}>
+            <Marketplace />
+            {/* any other components */}
+          </div>
+          <Footer />
+        </div>
   </div>
   
 }
